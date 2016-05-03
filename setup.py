831fd7c472351b8af1e34a4c7754d2348c1dd107
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2016-05-03 11:49:07 -0400

    Add setup.cfg file.  Fix url and author emails in setup.py.  Add MIT license to LICENSE file.  Minor version bump.

--- setup.py
+++ setup.py
@@ -2,25 +2,27 @@ import os
 from setuptools import (find_packages, setup)
 
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as v_file:
-    version = v_file.read().strip()
+    VERSION = v_file.read().strip()
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as r_file:
+    README = r_file.read().strip()
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='canvas-oauth',
-    version=version,
-    packages=find_packages(),
-    include_package_data=True,
-    license="License :: OSI Approved :: MIT License",
+    version=VERSION,
+    url="https://github.com/Harvard-University-iCommons/django-canvas-oauth",
+    author="Jaime Bermudez",
+    author_email="tlt-ops@g.harvard.edu, jaime_bermudez@harvard.edu",
     description='A reusable Django app used to handle OAuth2 flow with Canvas.',
     long_description=README,
-    url='https://www.example.com/',
-    author='Jaime Bermudez',
-    author_email='tlt-ops@g.harvard.edu',
+    license="License :: OSI Approved :: MIT License",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
